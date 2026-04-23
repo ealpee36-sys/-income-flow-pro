9f06eed27febefc17e0589380c947530114763be
ealpee36-sys <ealpee36@gmail.com>
2026-04-23 17:42:55 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,24 @@
-const CACHE_NAME = "income-flow-cache";
+const CACHE_NAME = "income-flow-v2";
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/styles.css",
-        "/app.js"
-      ]);
-    })
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
